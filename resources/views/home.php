ee946bbc5b9f9b5b33b3763dca979b9c91f5db19
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-05-19 20:38:36 +0100

    Version 1.1

---
+++ resources/views/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .button {
+            position: absolute;
+            top: 50px; 
+            left: 50px;
+            text-align: center;
+            align-items: center;
+            justify-content: center;
+            color: white;
+            background: red;
+            display: inline-flex;
+            border: none;
+            border-radius: 20%;
+            font-size: 60%;
+            padding: 1%;
+        }
+    </style>
+</head>
+<body style="color: blue;">
+    <button type="button" class="button"><b> Enviar</b></button>    
+</body>
+</html>
