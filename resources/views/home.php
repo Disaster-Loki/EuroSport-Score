8e4b0691dcae3bda79306ed5971d74217cf0ff9d
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-05-21 21:14:25 +0100

    Version 1.1

--- resources/views/home.php
+++ resources/views/home.php
@@ -4,27 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/home.css">
-    <style>
-        * {
-    margin: 0;
-    padding: 0;
-}
-.button {
-    position: absolute;
-    top: 50px; 
-    left: 50px;
-    text-align: center;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    background: red;
-    display: inline-flex;
-    border: none;
-    border-radius: 20%;
-    font-size: 60%;
-    padding: 1%;
-}
-    </style>
     <title>Home</title>
 </head>
 <body>
