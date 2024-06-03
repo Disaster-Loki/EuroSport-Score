f706a0ed7e88a7ff49c2071535d304ff3b5a9ab0
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-06-03 20:01:40 +0100

    Version 1.2

---
+++ resources/views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/style-login.css">
+</head>
+<body>
+    <form action="" method="get" class="form-login">
+        <input type="email" value="" placeholder="Email">
+        <input type="password" value="" placeholder="Password">
+        <input type="submit" value="" placeholder="LOGIN">
+    </form>
+    
+</body>
+</html>
